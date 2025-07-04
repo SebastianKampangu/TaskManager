30ab508ec32b431860e827f5c3c9efa7b8400c9d
Sebastien Kampangu <sebastiankampangu@gmail.com>
2025-07-04 19:28:04 +1000

    Initial Task Manager project setup.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TaskManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to task Manager!");
+            //TODO: Create TaskManager instance
+            //TODO: Implement main program LOOP
+            //TODO: Display menu options
+            //TODO: Handle user input
+
+            Console.WriteLine("Thank you for using Task Manager!");
+            Console.ReadKey();
+        }
+    }
+}
