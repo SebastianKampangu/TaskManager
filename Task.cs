225f48507472448f4b045ac7dd9d8b62d888b334
Sebastien Kampangu <sebastiankampangu@gmail.com>
2025-07-05 23:58:37 +1000

    Implimete Task properties

--- Task.cs
+++ Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,11 @@ namespace TaskManager
     internal class Task
     {
         //TODO: Add properties for task data
+        //Task properties using appropriate data Tapes
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DueDate { get; set; }
         //TODO: Add Constructor to create new tskd
         //TODO: Add methods for task operations
         //TODO: Override ToSpring() for display formatting
